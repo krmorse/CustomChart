dc7bb47738e543051160a003a392940931420997
Kyle Morse <kmorse@rallydev.com>
2016-06-02 15:11:44 -0600

    First pass at settings

--- CustomChartApp.js
+++ CustomChartApp.js
@@ -7,19 +7,134 @@ Ext.define('CustomChartApp', {
     config: {
         defaultSettings: {
             types: 'defect',
-            chartType: 'barchart',
-            aggregationField: 'State',
-            aggregationType: 'count'
+            chartType: 'piechart',
+            aggregationField: 'ScheduleState',
+            aggregationType: 'count',
+            query: ''
         }
     },
 
     launch: function() {
-        Rally.data.wsapi.ModelFactory.getModels({
-            types: this._getTypesSetting()
-        }).then({
-            success: this._onModelsLoaded,
-            scope: this
-        });
+        if (!this.getSetting('types')) {
+            this.fireEvent('appsettingsneeded'); //todo: does this work?
+        } else {
+            Rally.data.wsapi.ModelFactory.getModels({
+                types: this._getTypesSetting()
+            }).then({
+                success: this._onModelsLoaded,
+                scope: this
+            });
+        }
+    },
+
+    getSettingsFields: function() {
+        return [
+            {
+                name: 'chartType',
+                xtype: 'rallycombobox',
+                plugins: ['rallyfieldvalidationui'],
+                fieldLabel: 'Chart Type',
+                displayField: 'name',
+                valueField: 'value',
+                editable: false,
+                allowBlank: false,
+                store: Ext.create('Ext.data.Store', {
+                    fields: ['name', 'value'],
+                    data: [
+                        { name: 'Pie', value: 'piechart' },
+                        { name: 'Bar', value: 'barchart' }
+                    ]
+                })
+            },
+            {
+                name: 'types',
+                xtype: 'rallycombobox',
+                plugins: ['rallyfieldvalidationui'],
+                allowBlank: false,
+                autoSelect: false,
+                // multiSelect: true, //todo: need to validate either all artifacts chosen or only one non-artifact
+                shouldRespondToScopeChange: true,
+                context: this.getContext(),
+                // initialValue: ['HierarchicalRequirement'], //todo: not working
+                storeConfig: {
+                    model: 'TypeDefinition',
+                    sorters: [{ property: 'DisplayName' }],
+                    fetch: ['DisplayName', 'TypePath'],
+                    filters: [{ property: 'UserListable', value: true }],
+                    autoLoad: false,
+                    remoteSort: false,
+                    sortOnLoad: true,
+                    remoteFilter: true
+                },
+                displayField: 'DisplayName',
+                valueField: 'TypePath',
+                listeners: {
+                    change: function (combo) {
+                        combo.fireEvent('typeselected', combo.getValue(), combo.context);
+                    }
+                },
+                bubbleEvents: ['typeselected'],
+                readyEvent: 'ready',
+                handlesEvents: {
+                    projectscopechanged: function (context) {
+                        this.refreshWithNewContext(context);
+                    }
+                }
+            },
+            {
+                name: 'aggregationField', //todo: don't validate on settings load
+                xtype: 'rallyfieldcombobox',
+                plugins: ['rallyfieldvalidationui'],
+                fieldLabel: 'Aggregate By',
+                readyEvent: 'ready',
+                allowBlank: false,
+                handlesEvents: {
+                    typeselected: function (models, context) {
+                        var type = Ext.Array.from(models)[0];
+                        if (type) {
+                            this.refreshWithNewModelType(type, context); //todo: how to handle multiple models
+                        } else {
+                            this.store.removeAll();
+                            this.reset();
+                        }
+                    }
+                },
+                listeners: {
+                    ready: function (combo) {
+                        combo.store.filterBy(function (record) {
+                            var field = record.get('fieldDefinition'),
+                                attr = field.attributeDefinition;
+                            return attr && !attr.Hidden && attr.AttributeType !== 'COLLECTION' &&
+                                !field.isMappedFromArtifact;
+                        });
+                        var fields = Ext.Array.map(combo.store.getRange(), function (record) {
+                            return record.get(combo.getValueField());
+                        });
+                        if (!Ext.Array.contains(fields, combo.getValue())) {
+                            combo.setValue(fields[0]);
+                        }
+                    }
+                }
+            },
+            {
+                name: 'aggregationType',
+                xtype: 'rallycombobox',
+                plugins: ['rallyfieldvalidationui'],
+                fieldLabel: 'Aggregation Type',
+                displayField: 'name',
+                valueField: 'value',
+                editable: false,
+                allowBlank: false,
+                store: Ext.create('Ext.data.Store', {
+                    fields: ['name', 'value'],
+                    data: [
+                        { name: 'Count', value: 'count' },
+                        { name: 'Plan Estimate', value: 'estimate' }
+                    ]
+                })
+            },
+            { type: 'query' }
+        ];
     },
 
     _onModelsLoaded: function(models) {
@@ -28,7 +143,6 @@ Ext.define('CustomChartApp', {
             modelNames = _.pluck(this.models, 'typePath'),
             gridBoardConfig = {
                 xtype: 'rallygridboard',
-                stateful: false,
                 toggleState: 'chart',
                 chartConfig: this._getChartConfig(),
                 plugins: [{
@@ -85,13 +199,26 @@ Ext.define('CustomChartApp', {
     _getChartConfig: function() {
         return {
             xtype: this.getSetting('chartType'),
-            chartColors: ['red', 'blue', 'green', 'yellow'],
-            storeType: 'Rally.data.wsapi.artifact.Store',
+            storeType: 'Rally.data.wsapi.artifact.Store', //todo: only if artifact types selected
+            chartColors: [
+              "#FF8200", // $orange
+              "#F6A900", // $gold
+              "#FAD200", // $yellow
+              "#8DC63F", // $lime
+              "#1E7C00", // $green_dk
+              "#337EC6", // $blue_link
+              "#005EB8", // $blue
+              "#7832A5", // $purple,
+              "#DA1884",  // $pink,
+              "#C0C0C0" // $grey4
+            ],
             storeConfig: {
                 models: this._getTypesSetting(),
                 context: this.getContext().getDataContext(),
+                //TODO: can we do summary fetch here and not limit infinity?
+                //we'll have to also make sure the fetch is correct for export somehow...
                 limit: Infinity,
-                fetch: ['FormattedID', 'Name', 'PlanEstimate', 'Priority', 'State']
+                fetch: ['FormattedID', 'Name', 'PlanEstimate', this.getSetting('aggregationField')]
             },
             calculatorConfig: {
                 calculationType: this.getSetting('aggregationType'),
