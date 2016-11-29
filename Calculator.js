d931e0b287db6019c6d8e90e42ab9f71ae30def0
John Martin <curmudgeon+git@rallydev.com>
2016-11-29 07:57:50 -0800

    if the category is '', show 'None' as the label.

--- Calculator.js
+++ Calculator.js
@@ -2,7 +2,12 @@ Ext.define('Calculator', {
 
     config: {
         calculationType: undefined,
-        field: undefined
+        field: undefined,
+        /*
+         * {Boolean} useNoneForBlank
+         * If true, show "None" for categories where the value is "".
+         */
+        useNoneForBlank: false
     },
 
     constructor: function(config) {
@@ -12,6 +17,9 @@ Ext.define('Calculator', {
     prepareChartData: function(store) {
         var data = _.groupBy(store.getRange(), function(record) {
             var value = record.get(this.field);
+            if ( Ext.isEmpty(value) && this.useNoneForBlank ) {
+                value = "None";
+            }
             return _.isObject(value) ? value._refObjectName : value;
         }, this),
         categories = _.keys(data),
