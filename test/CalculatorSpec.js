6e51dc4bc72b4560ff1951554a77a7b924c7376a
Kyle Morse <kmorse@rallydev.com>
2016-12-04 14:30:53 -0700

    Add remaining calculator tests

--- test/CalculatorSpec.js
+++ test/CalculatorSpec.js
@@ -4,6 +4,14 @@ describe('Calculator', function() {
 
         var data, store;
 
+        function expectChartDataToBe(chartData, expectedSeriesData) {
+            expect(chartData.categories).toEqual(['P1', 'P2', 'P3', 'P4']);
+            expect(chartData.series.length).toBe(1);
+            var seriesData = chartData.series[0];
+            expect(seriesData.name).toBe('Priority');
+            expect(seriesData.data).toEqual(expectedSeriesData);
+        }
+
         beforeEach(function() {
             var model = Rally.test.Mock.dataFactory.getModel('defect');
             data = Rally.test.Mock.dataFactory.getRecords('defect', {
@@ -27,11 +35,7 @@ describe('Calculator', function() {
                 calculationType: 'count'
             });
             var chartData = calculator.prepareChartData(store);
-            expect(chartData.categories).toEqual(['P1', 'P2', 'P3', 'P4']);
-            expect(chartData.series.length).toBe(1);
-            var seriesData = chartData.series[0];
-            expect(seriesData.name).toBe('Priority');
-            expect(seriesData.data).toEqual([['P1', 1], ['P2', 1], ['P3', 1], ['P4', 1]]);
+            expectChartDataToBe(chartData, [['P1', 1], ['P2', 1], ['P3', 1], ['P4', 1]]);
         });
 
         it('should aggregate by plan estimate', function() {
@@ -40,11 +44,7 @@ describe('Calculator', function() {
                 calculationType: 'estimate'
             });
             var chartData = calculator.prepareChartData(store);
-            expect(chartData.categories).toEqual(['P1', 'P2', 'P3', 'P4']);
-            expect(chartData.series.length).toBe(1);
-            var seriesData = chartData.series[0];
-            expect(seriesData.name).toBe('Priority');
-            expect(seriesData.data).toEqual([['P1', 2], ['P2', 3], ['P3', 4], ['P4', 5]]);
+            expectChartDataToBe(chartData, [['P1', 2], ['P2', 3], ['P3', 4], ['P4', 5]]);
         });
 
         it('should bucket by object type', function() {
@@ -66,5 +66,96 @@ describe('Calculator', function() {
         });
     });
 
-    //TODO: add tests for feature types
+    describe('with features', function() {
+
+        var data, store;
+
+        function expectChartDataToBe(chartData, expectedSeriesData) {
+            expect(chartData.categories).toEqual(['UX', 'Maintenance', 'Product Roadmap', 'Architecture Roadmap']);
+            expect(chartData.series.length).toBe(1);
+            var seriesData = chartData.series[0];
+            expect(seriesData.name).toBe('InvestmentCategory');
+            expect(seriesData.data).toEqual(expectedSeriesData);
+        }
+
+        beforeEach(function() {
+            var model = Rally.test.Mock.dataFactory.getModel('portfolioitem/feature');
+            data = Rally.test.Mock.dataFactory.getRecords('portfolioitem/feature', {
+                count: 4,
+                values: [
+                    { InvestmentCategory: 'UX', PreliminaryEstimateValue: 2, Parent: null, AcceptedLeafStoryCount: 3, AcceptedLeafStoryPlanEstimateTotal: 4, LeafStoryCount: 5, LeafStoryPlanEstimateTotal: 6 },
+                    { InvestmentCategory: 'Maintenance', PreliminaryEstimateValue: 3, Parent: { _refObjectName: 'Initiative1' }, AcceptedLeafStoryCount: 4, AcceptedLeafStoryPlanEstimateTotal: 5, LeafStoryCount: 6, LeafStoryPlanEstimateTotal: 7 },
+                    { InvestmentCategory: 'Product Roadmap', PreliminaryEstimateValue: 4, Parent: { _refObjectName: 'Initiative2' }, AcceptedLeafStoryCount: 5, AcceptedLeafStoryPlanEstimateTotal: 6, LeafStoryCount: 7, LeafStoryPlanEstimateTotal: 8 },
+                    { InvestmentCategory: 'Architecture Roadmap', PreliminaryEstimateValue: 5, Parent: { _refObjectName: 'Initiative3' }, AcceptedLeafStoryCount: 6, AcceptedLeafStoryPlanEstimateTotal: 7, LeafStoryCount: 8, LeafStoryPlanEstimateTotal: 9 }
+                ]
+            });
+            store = Ext.create('Rally.data.wsapi.Store', {
+              model: model,
+              data: data
+            });
+        });
+
+        it('should aggregate by count', function() {
+            var calculator = Ext.create('Calculator', {
+                field: 'InvestmentCategory',
+                calculationType: 'count'
+            });
+            var chartData = calculator.prepareChartData(store);
+            expectChartDataToBe(chartData, [['UX', 1], ['Maintenance', 1], ['Product Roadmap', 1], ['Architecture Roadmap', 1]]);
+        });
+
+        it('should aggregate by preliminary estimate', function() {
+          var calculator = Ext.create('Calculator', {
+              field: 'InvestmentCategory',
+              calculationType: 'prelimest'
+          });
+          var chartData = calculator.prepareChartData(store);
+          expectChartDataToBe(chartData, [['UX', 2], ['Maintenance', 3], ['Product Roadmap', 4], ['Architecture Roadmap', 5]]);
+        });
+
+        it('should aggregate by accepted leaf count', function() {
+          var calculator = Ext.create('Calculator', {
+              field: 'InvestmentCategory',
+              calculationType: 'acceptedleafcount'
+          });
+          var chartData = calculator.prepareChartData(store);
+          expectChartDataToBe(chartData, [['UX', 3], ['Maintenance', 4], ['Product Roadmap', 5], ['Architecture Roadmap', 6]]);
+        });
+
+        it('should aggregate by accepted leaf story plan estimate total', function() {
+          var calculator = Ext.create('Calculator', {
+              field: 'InvestmentCategory',
+              calculationType: 'acceptedleafplanest'
+          });
+          var chartData = calculator.prepareChartData(store);
+          expectChartDataToBe(chartData, [['UX', 4], ['Maintenance', 5], ['Product Roadmap', 6], ['Architecture Roadmap', 7]]);
+        });
+
+        it('should aggregate by leaf story count', function() {
+          var calculator = Ext.create('Calculator', {
+              field: 'InvestmentCategory',
+              calculationType: 'leafcount'
+          });
+          var chartData = calculator.prepareChartData(store);
+          expectChartDataToBe(chartData, [['UX', 5], ['Maintenance', 6], ['Product Roadmap', 7], ['Architecture Roadmap', 8]]);
+        });
+
+        it('should aggregate by leaf story plan estimate total', function() {
+          var calculator = Ext.create('Calculator', {
+              field: 'InvestmentCategory',
+              calculationType: 'leafplanest'
+          });
+          var chartData = calculator.prepareChartData(store);
+          expectChartDataToBe(chartData, [['UX', 6], ['Maintenance', 7], ['Product Roadmap', 8], ['Architecture Roadmap', 9]]);
+        });
+
+        it('should bucket by object type', function() {
+            var calculator = Ext.create('Calculator', {
+                field: 'Parent',
+                calculationType: 'count'
+            });
+            var chartData = calculator.prepareChartData(store);
+            expect(chartData.categories).toEqual(['None', 'Initiative1', 'Initiative2', 'Initiative3']);
+        });
+    });
 });
